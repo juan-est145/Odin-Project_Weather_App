099965544df376685e6e666bfe54b6a872f583fa
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-16 18:58:37 +0200

    Changed a bit the name of the messages

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { getData,} from "./getWeatherData";
 import { IWeather, IWeatherError } from "./mainInterfaces";
 
-const result = getData("Jaén");
+const result = getData("Sevilla");
 
 result.then((fulfilled: IWeather | IWeatherError) => {
 	if (typeof fulfilled === "object" && "error" in fulfilled) {
 		alert("Could not find location, try again");
 		return;
 	}
-	console.log(`The time in ${fulfilled.location.name}, is of ${fulfilled.current.temp_c} ºC`);
+	console.log(`The time in ${fulfilled.location.name} is ${fulfilled.current.temp_c} ºC`);
 	console.log(`The conditon is expected to be ${fulfilled.current.condition.text}`);
 	console.log(`${fulfilled.forecast.forecastday.forEach(day =>{
 		console.log(`For ${day.date}, the average temperature will be ${day.day.avgtemp_c}`);
