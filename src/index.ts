ca350e485d2ac65a0818bdc493c6313c20992460
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-16 14:16:45 +0200

    Interfaces are working

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ result.then((fullfilled: IWeatherData) =>
 {
 	Object.keys(fullfilled).forEach((key) => console.log(key));
 	console.log(fullfilled);
-	console.log(fullfilled.location.name)
+	console.log(fullfilled.location.name);
+	console.log(fullfilled.current.condition.text);
 });
 
 
