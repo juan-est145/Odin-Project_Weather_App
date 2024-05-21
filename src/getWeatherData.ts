f875c9b8da5c135254641a7984798cdf216f8ef7
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-21 19:00:36 +0200

    Error is finally over, now it prevents default behaviour with form

--- src/getWeatherData.ts
+++ src/getWeatherData.ts
@@ -1,12 +1,29 @@
-export async function getData(location: string): Promise<Object> {
+import { IWeather, IWeatherError } from "./mainInterfaces";
+
+export async function sendCityInfo() {
+	const searchBar: HTMLInputElement = document.querySelector("#search-bar");
+	const result: IWeather | IWeatherError = await getData(searchBar.value);
+	if (typeof result === "object" && "error" in result) {
+		alert("Could not find location, try again");
+		return;
+	}
+	console.log(`The time in ${result.location.name} is ${result.current.temp_c} ºC`);
+	console.log(`The conditon is expected to be ${result.current.condition.text}`);
+	console.log(`${result.forecast.forecastday.forEach(day => {
+		console.log(`For ${day.date}, the average temperature will be ${day.day.avgtemp_c}`);
+	})}`);
+}
+
+
+async function getData(location: string): Promise<IWeather | IWeatherError> {
 	try {
 		const key: string = "e7ced9d94cd541f5a8e184800241405";
 		const response: Response = await fetch(
 			`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=3&aqi=no&alerts=no&hour=9`
 			, { mode: 'cors' }
-		);
-		const weatherData: Object = await response.json();
-		return (weatherData);
+		)
+		const weatherData: IWeather | IWeatherError = await response.json();
+		return weatherData;
 	}
 	catch (error) {
 		alert("There was an error retrieving the data, please try again later");
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-import { getData,} from "./getWeatherData";
-import { IWeather, IWeatherError } from "./mainInterfaces";
+import { sendCityInfo,} from "./getWeatherData";
 import "./style.css"
 
-const result = getData("Sevilla");
+//const result = getData("Sevilla");
 
-/*result.then((fulfilled: IWeather | IWeatherError) => {
-	if (typeof fulfilled === "object" && "error" in fulfilled) {
-		alert("Could not find location, try again");
-		return;
-	}
-	console.log(`The time in ${fulfilled.location.name} is ${fulfilled.current.temp_c} ºC`);
-	console.log(`The conditon is expected to be ${fulfilled.current.condition.text}`);
-	console.log(`${fulfilled.forecast.forecastday.forEach(day =>{
-		console.log(`For ${day.date}, the average temperature will be ${day.day.avgtemp_c}`);
-	})}`);
-});*/
+const searchButton = document.querySelector("#search");
+searchButton.addEventListener("click", async (e) => {
+	e.preventDefault();
+	await sendCityInfo();
+});
+
